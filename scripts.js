a1dd5e93e8dd372ec3b751dd3e4dc8df3bef8077
ilya pasdetemps <ilya.pasdetemps@gmail.com>
2025-04-09 20:48:08 +0400

    next iteration

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
-function loadStory(filename) {
+function loadStory(filename, title) {
     fetch(filename)
         .then(response => response.text())
         .then(text => {
             document.getElementById('main-page').style.display = 'none';
             document.getElementById('story-page').style.display = 'block';
+            document.getElementById('story-title').innerText = title;
             document.getElementById('story-content').innerText = text;
         })
         .catch(error => console.error('Error loading story:', error));
@@ -11,9 +12,16 @@ function loadStory(filename) {
 
 function toggleMenu() {
     const menu = document.getElementById('story-menu');
+    const burger = document.querySelector('.burger');
+    const storyTitle = document.getElementById('story-title');
+
     if (menu.style.display === 'block') {
         menu.style.display = 'none';
+        burger.classList.remove('open');
+        storyTitle.innerText = currentStoryTitle; // Restore the story title
     } else {
         menu.style.display = 'block';
+        burger.classList.add('open');
+        storyTitle.innerText = 'Author Name'; // Change to author name
     }
 }
